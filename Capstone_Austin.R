5b6b4968bdbb76609962e0e1270f93b946ac94a6
bmac1313 <brianmac1313@gmail.com>
2016-10-23 21:58:48 -0500

    Data cleaning plus merge
    Additional data cleaning for Zillow and ACS  data and merged Jurisdictions with zip codes for ACS data

--- Capstone_Austin.R
+++ Capstone_Austin.R
@@ -37,7 +37,7 @@ head(ZillowOwn,n=5)
 
 #Remove "X" from time series (year) column header
 setnames(ZillowOwn, old=c("CountyName", "X2011","X2012", "X2013", "X2014", "X2015"),
-         new=c("Coounty", "2011","2012", "2013", "2014", "2015"))
+         new=c("County", "2011","2012", "2013", "2014", "2015"))
 head(ZillowOwn, n=5)
 
 
@@ -59,32 +59,53 @@ ACS2014<-read.csv(file="ACS_14_5YR_S1903.csv", header=TRUE,sep=",")
 
 
 #Change Headers for all ACS .csv Files
-head(ACS2011,n=5)
 setnames(ACS2011, old=c("GEO.id", "GEO.id2","HC01_EST_VC02", "HC02_EST_VC02"),
-         new=c("Geoid1", "ZipCode","Total Est HH", "Med Income HH"))
+         new=c("Geoid1", "ZipCode","2011 Total Est HH", "2011 Med Income HH"))
+
 head(ACS2011,n=5)
 
-head(ACS2012,n=5)
 setnames(ACS2012, old=c("GEO.id", "GEO.id2","HC01_EST_VC02", "HC02_EST_VC02"),
-         new=c("Geoid1", "ZipCode","Total Est HH", "Med Income HH"))
+         new=c("Geoid1", "ZipCode","2012 Total Est HH", "2012 Med Income HH"))
+
 head(ACS2012,n=5)
 
-head(ACS2013,n=5)
 setnames(ACS2013, old=c("GEO.id", "GEO.id2","HC01_EST_VC02", "HC02_EST_VC02"),
-         new=c("Geoid1", "ZipCode","Total Est HH", "Med Income HH"))
+         new=c("Geoid1", "ZipCode","2013 Total Est HH", "2013 Med Income HH"))
+
 head(ACS2013,n=5)
 
-head(ACS2014,n=5)
 setnames(ACS2014, old=c("GEO.id", "GEO.id2","HC01_EST_VC02", "HC02_EST_VC02"),
-         new=c("Geoid1", "ZipCode","Total Est HH", "Med Income HH"))
+         new=c("Geoid1", "ZipCode","2014 Total Est HH", "2014 Med Income HH"))
+
 head(ACS2014,n=5)
+
+#Merge ACS time series dataframes into a single dataframe called ACStotal
+
+ACStotal_1 <- merge(x = ACS2013, y = ACS2014, by = "ZipCode", all = TRUE)
+
+ACStotal_2 <- merge(x = ACS2011, y = ACS2012, by = "ZipCode", all = TRUE)
+
+ACStotal <- merge(x = ACStotal_2, y = ACStotal_1, by = "ZipCode", all = TRUE)
+
+ACStotal$Geoid1.x.x <- NULL
+ACStotal$Geoid1.y.x <- NULL
+ACStotal$Geoid1.x.y <- NULL
+ACStotal$Geoid1.y.y <- NULL
+
+head(ACStotal, n=3)
 
 #DATA ANALYSIS
 
 #Summarize ACS Data using ggplot to view changes in data.
-#Merge Datasets into one
 
-#Graph data using ggplot
+#Add City, Metro and County data to ACS data, from Zillow Data 
+ACStotal_geo <- merge(x = ACStotal, y = ZillowOwn, by = "ZipCode", all = TRUE)
+head(ACStotal_geo, n=5)
 
+#Remove unneccesary columns
+ACStotal_geo$SizeRank <- ACStotal_geo$`2011` <- ACStotal_geo$`2012`  <- ACStotal_geo$`2013`  <- ACStotal_geo$`2014`  <- ACStotal_geo$`2015` <- NULL
+head(ACStotal_geo, n=5)
 
-
+ggplot(data=ACStotal_geo,
+       aes(x=date, y=value, colour=variable)) +
+  geom_line()
